ac73c0a5d4829d397196d8e4e9e86f4c65f2bade
Greg Brant <greg@gregbrant.com>
2017-10-04 21:28:39 +0100

    Added SparseDictionary

---
+++ GregBrant.Utils/Collections/SparseDictionary.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GregBrant.Utils.Collections
+{
+    public class SparseDictionary<TKey, TValue> : Dictionary<TKey, TValue>
+    {
+        private readonly Func<TKey, TValue> _valueGetter;
+
+        public SparseDictionary() : this(_ => default(TValue))
+        {
+
+        }
+
+        public SparseDictionary(Func<TKey, TValue> valueGetter)
+        {
+            _valueGetter = valueGetter;
+        }
+
+        public new TValue this[TKey key]
+        {
+            get
+            {
+                if (ContainsKey(key) == false)
+                {
+                    base[key] = _valueGetter(key);
+                }
+
+                return base[key];
+            }
+            set { base[key] = value; }
+        }
+    }
+}
